3a060324e30a42d0a1f14db90f9e309ac0249e5a
TheNamlessGuy <bragux@hotmail.com>
2017-05-05 21:48:39 +0200

    Gspace made, added spacestate

---
+++ Gspace/__init__.py
@@ -0,0 +1,1 @@
+from WorldInterface import WorldInterface
---
+++ SpaceState.py
@@ -0,0 +1,11 @@
+from Gspace import WorldInterface
+
+class SpaceState(WorldInterface):
+    def __init__(self):
+        WorldInterface.__init__(self)
+
+    def update(self, game_state={}):
+        pass
+
+    def onLoad(self, game_state={}):
+        pass
