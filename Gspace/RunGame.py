0f15556b097bdc4bad428a03485d816a35d6f718
GoranEsmail <GoranEsmail@users.noreply.github.com>
2017-05-06 13:56:11 +0200

    Update and rename RunGame to RunGame.py
    switch world interface by returning the new one

--- Gspace/RunGame.py
+++ Gspace/RunGame.py
@@ -1,5 +1,6 @@
 #encoding: utf-8
 
+import pygame
 import WorldInterface
 
 #import graphics
@@ -7,20 +8,32 @@ import WorldInterface
 #dummy until graphics is implemented
 
 def draw_world( world ):
-    pass
+    print "no draw implementation yet"
 
 def run_game( world, game_state = {}, fps = 24 ):
+    
+    pygame.init()
+
+    screen = pygame.display.set_mode((640, 480))
+    back_colour = ( 255, 0, 0 )
+    background = pygame.Surface( screen.get_size() ).fill( back_colour ).convert()
+    owl = (0,0)
+    screen.blit( background, owl )
     
     timer = pygame.time.Clock()
     world.reset()
     
-    ret = None
-    while ret == None:
+    game_state["screen"] = screen
+    game_state["clock"] = timer
+    game_state["running"] = True
+    
+    while game_state["running"]:
         ret = WorldInterface.update( world, game_state )
         if isinstance( ret, WorldInterface ):
             world = ret
             ret = None
             world.reset( game_state )
+            continue
         
         # draw to buffer
         draw_world( world )
@@ -28,3 +41,5 @@ def run_game( world, game_state = {}, fps = 24 ):
         pygame.display.flip()
         # sync
         timer.tick( fps )
+
+
