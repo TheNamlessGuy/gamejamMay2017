80570d29a748fd1c6777d5fa0e40d82d69cb4155
Henrik Ingbrant Björs <inhuman1995@gmail.com>
2017-05-06 22:07:17 +0200

    Main changes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PlanetState import *
 from MeteorState import *
 from GameOverState import *
 from TransitionCutsceneState import *
+from SpoonCutsceneState import *
 
 if __name__ == '__main__':
     game_state = {}
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     game_state['world-planet'] = PlanetState()
     game_state['world-meteor'] = MeteorState()
     game_state['world-gameover'] = GameOverState()
-    game_state['world-cutscene'] = {'falling': TransitionCutsceneState()}
+    game_state['world-cutscene'] = {'falling': TransitionCutsceneState(), 'spoon-expansion': SpoonCutsceneState()}
     game_state['camera'] = Vec2( 0.0, 0.0 )
     game_state['spoon-pwr'] = 0
     run_game( game_state['world-space'], game_state, 24 )
