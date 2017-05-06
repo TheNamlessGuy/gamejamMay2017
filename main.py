9afb937c50efbc8035a9e765a8abcf6b890e33f1
GoranEsmail <GoranEsmail@users.noreply.github.com>
2017-05-06 13:51:04 +0200

    Update main.py
    main should work but needs a world to start from

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import pygame
-pygame.init()
+#encoding: utf-8
 
-screen = pygame.display.set_mode((640, 480))
+from Gspace import *
 
-background = pygame.Surface(screen.get_size())
-background.fill((255,0,0))
-background = background.convert()
+if __name__ == '__main__':
+    game_state = {}
+    world = None # (Macs) TODO: fixa denna rad!!!
+    run_game( world, game_state, 24 )
 
-screen.blit(background, (0,0))
-
-mainloop = True
-
+"""
+# gammal kod! använd denna event-loop vid override av WorldInterface i update-metoden!!!
+# TODO: ta bort kommentaren efter att ni har implementerat detta
 while mainloop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,3 +19,4 @@ while mainloop:
                 mainloop = False
     
     pygame.display.flip()
+"""
