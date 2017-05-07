6ab4c32ab443f6ddfe5afb49973a523be3f90562
TheNamlessGuy <bragux@hotmail.com>
2017-05-07 12:44:28 +0200

    Refactor keyboard

--- Gspace/Keyboard.py
+++ Gspace/Keyboard.py
@@ -3,12 +3,14 @@
 class Keyboard:
     def __init__(self, pygame):
         self.keys_down = {}
+        self.key_variables = {}
 
-        self.up_keys = [pygame.K_UP, pygame.K_w]
-        self.down_keys = [pygame.K_DOWN, pygame.K_s]
-        self.left_keys = [pygame.K_LEFT, pygame.K_a]
-        self.right_keys = [pygame.K_RIGHT, pygame.K_d]
-        self.action_keys = [pygame.K_SPACE]
+        self.key_variables['ctrl-up'] = [pygame.K_UP, pygame.K_w]
+        self.key_variables['ctrl-down'] = [pygame.K_DOWN, pygame.K_s]
+        self.key_variables['ctrl-left'] = [pygame.K_LEFT, pygame.K_a]
+        self.key_variables['ctrl-right'] = [pygame.K_RIGHT, pygame.K_d]
+        self.key_variables['ctrl-action'] = [pygame.K_SPACE]
+        self.key_variables['ctrl-debug'] = [pygame.K_0]
 
         self.keys_down['ctrl-up'] = 0
         self.keys_down['ctrl-down'] = 0
@@ -22,18 +24,10 @@ class Keyboard:
             if event.type == pygame.QUIT: return False
 
             if event.type != pygame.KEYDOWN and event.type != pygame.KEYUP: continue
-            if event.key in self.up_keys:
-                self.keys_down['ctrl-up'] += 1 if event.type == pygame.KEYDOWN else -1
-            elif event.key in self.down_keys:
-                self.keys_down['ctrl-down'] += 1 if event.type == pygame.KEYDOWN else -1
-            elif event.key in self.left_keys:
-                self.keys_down['ctrl-left'] += 1 if event.type == pygame.KEYDOWN else -1
-            elif event.key in self.right_keys:
-                self.keys_down['ctrl-right'] += 1 if event.type == pygame.KEYDOWN else -1
-            elif event.key in self.action_keys:
-                self.keys_down['ctrl-action'] += 1 if event.type == pygame.KEYDOWN else -1
-            elif event.key == pygame.K_0:
-                self.keys_down['ctrl-debug'] += 1 if event.type == pygame.KEYDOWN else -1
+            for key in self.key_variables:
+                if event.key in self.key_variables[key]:
+                    self.keys_down[key] += 1 if event.type == pygame.KEYDOWN else -1
+                    break
 
         return True
 
